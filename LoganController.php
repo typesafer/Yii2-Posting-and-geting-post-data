90b501d05b9865593a3f66a164eeb09582baa93f
Geoffrey <thismuller@gmail.com>
2015-01-27 20:19:14 +0300

    Update LoganController.php

--- LoganController.php
+++ LoganController.php
@@ -4,14 +4,14 @@ namespace app\controllers;
 use Yii;
 use yii\helpers\Url;
 use yii\web\Controller;
-use app\models\LoganForm;
+use app\models\LoganModel;
 
 
 class LoganController extends Controller
 {
 
 	public function actionHello(){
-	$model = new LoganForm;
+	$model = new LoganModel;
 	 if ($model->load(Yii::$app->request->post()) && $model->validate()) {
 	 $name = $model->name;
 	 Yii::$app->session->setFlash("success","You have entered data correctly $name");
